75bc5298e2827bfd8edaef88ef738ee93add8af4
Zubair <A.N.F.Al-Zubi@student.tudelft.nl>
2023-04-28 15:08:45 +0200

    module2 audio measuring setup

--- KittSensor.py
+++ KittSensor.py
@@ -6,8 +6,8 @@ for i in range(pyaudio_handle.get_device_count()):
     device_info = pyaudio_handle.get_device_info_by_index(i)
     print(i, device_info['name'])
 
-Fs = 48000
-device_index = ?
+Fs = 48000  # Sampling freq
+device_index = device_info[:]  # Chosen device index
 stream = pyaudio_handle.open(input_device_index=device_index,
     channels=5,
     format=pyaudio.paInt16,
