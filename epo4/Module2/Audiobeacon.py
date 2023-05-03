cd3e314caf37aae1d5a955716b83f5dea266fb31
Zubair <zubair.alzubi@outlook.com>
2023-05-03 11:52:21 +0200

    updated

--- epo4/Module2/Audiobeacon.py
+++ epo4/Module2/Audiobeacon.py
@@ -21,7 +21,7 @@ serial_port.write(b'F' + carrier_frequency + b'\n')
 bit_frequency = (1000).to_bytes(2, byteorder='big')
 serial_port.write(b'B' + bit_frequency + b'\n')
 
-# Repetition count = bit freq / repitition freq
+# Repetition count = bit freq / repetition freq
 repetition_count = (10).to_bytes(2, byteorder='big')
 serial_port.write(b'R' + repetition_count + b'\n')
 
@@ -29,7 +29,7 @@ code = 0xDEADBEEF.to_bytes(4, byteorder='big')
 serial_port.write(b'C' + code + b'\n')
 time.sleep(10)
 
-serial_port.write(b'A0\n') # off
+serial_port.write(b'A0\n')  # off
 time.sleep(1)
 # close connection
 serial_port.close()
