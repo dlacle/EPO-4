1ff0cb027311bb8d20a267879939fdecc23ce59b
Zubair <zubair.alzubi@outlook.com>
2023-05-03 12:05:17 +0200

    added audiobeacon

---
+++ AudioBeacon.py
@@ -0,0 +1,36 @@
+import serial
+import time
+
+from serial import to_bytes
+
+# transmitting connection takes place over port 6
+comport = 'COM7'
+
+# getting access to bluetooth link
+serial_port = serial.Serial(comport, 115200, rtscts=True)
+
+
+# Audio beacon command
+serial_port.write(b'A1\n')  # on
+
+
+# Carrier freq = 20 kHz
+carrier_frequency = (20000).to_bytes(2, byteorder='big')
+serial_port.write(b'F' + carrier_frequency + b'\n')
+
+# Bit freq = 5 kHz
+bit_frequency = (5000).to_bytes(2, byteorder='big')
+serial_port.write(b'B' + bit_frequency + b'\n')
+
+# Repetition count = bit freq / repitition freq
+repetition_count = (2500).to_bytes(2, byteorder='big')
+serial_port.write(b'R' + repetition_count + b'\n')
+
+code = 0xDEADBEEF.to_bytes(4, byteorder='big')
+serial_port.write(b'C' + code + b'\n')
+time.sleep(10)
+
+serial_port.write(b'A0\n') # off
+
+# close connection
+serial_port.close()
