e37b44c53eba4d5805c243e0ec18847a85f446a6
Samh2oman <s.waterman@student.tudelft.nl>
2023-05-17 13:56:50 +0200

    Updated

--- ReferenceSignal.py
+++ ReferenceSignal.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # File path
-file_path = r"C:\Users\ZA\Desktop\EPO-4\EPO-4-Python\Mic-Data\data_mics_kitt_10cm_ref.txt"
+file_path = r"C:\Users\Sam\PycharmProjects\EPO-4\Mic-Data\data_mics_kitt_carrier_2250_bit_3k_ref.txt"
 
 # Load data from the text file
 data = np.loadtxt(file_path)
@@ -11,10 +11,10 @@ data = np.loadtxt(file_path)
 fs = 48000
 
 # Total duration
-t_total = 10
+t_total = len(data)/fs
 
 # Number of peaks
-num_peaks = 16
+num_peaks = 12
 
 # Calculate the number of samples per peak
 samples_per_peak = int(fs * t_total / num_peaks)
@@ -28,9 +28,61 @@ data_reshaped = data[:samples_per_peak * num_peaks].reshape((num_peaks, samples_
 # Calculate the average of all peaks to cancel the noise
 average_peak = np.mean(data_reshaped, axis=0)
 
+# Calculate autocorrelation
+autocorr = np.correlate(average_peak, average_peak, mode='full')
+
+# Time lag vector for autocorrelation
+t_autocorr = np.linspace(-t_total, t_total, len(autocorr))
+
+
+
+
+# Clean up the recording by removing zero intervals
+# clean_data = average_peak[np.nonzero(average_peak)]
+
+threshold = 1000 # Adjust this value according to your needs
+
+# Find the indices where the peak values exceed the threshold
+peak_indices = np.where(abs(average_peak)> threshold)[0]
+
+# Find the start and end indices of the non-zero interval
+start_index = peak_indices[0]
+end_index = peak_indices[-1]
+
+# Trim the average peak to remove the zero intervals
+clean_peak = average_peak[start_index:end_index+1]
+
+
 # Plot the reference signal (amplitude vs time) for the average peak
+plt.figure(figsize=(12, 4))
+plt.subplot(141)
 plt.plot(t, average_peak)
 plt.xlabel("Time (s)")
 plt.ylabel("Amplitude")
 plt.title("Reference Signal: Amplitude vs Time (Averaged Peak)")
+
+# Plot the autocorrelation
+plt.subplot(142)
+plt.plot(t_autocorr,autocorr)
+plt.xlabel("Time Lag (s)")
+plt.ylabel("Autocorrelation")
+plt.title("Autocorrelation of Average Peak")
+
+#plot clean data
+plt.subplot(143)
+# plt.plot(t[:len(clean_data)], clean_data)
+plt.plot(t[start_index:end_index+1], clean_peak)
+plt.xlabel("Time (s)")
+plt.ylabel("Amplitude")
+plt.title("Clean Single Pulse")
+
+plt.subplot(144)
+t_c = np.linspace(-t_total, t_total, len(clean_peak))
+C_c = np.correlate(clean_peak, clean_peak, mode='full')
+plt.plot(C_c)
+plt.xlabel("Time Lag (s)")
+plt.ylabel("Autocorrelation")
+plt.title("Autocorrelation of Average Peak")
+
+plt.tight_layout()
 plt.show()
--- goldcode autocorrolation.py
+++ goldcode autocorrolation.py
@@ -4,20 +4,17 @@ import matplotlib.pyplot as plt
 # Define the Gold code sequence
 hex_num = "3355A780"
 bin_num = bin(int(hex_num, 16))[2:]  # Convert hex to int, then to binary
-print(len(bin_num))
 seq = np.array([int(x) for x in bin_num])
-print(seq)
+
 # Compute the autocorrelation using numpy's correlate function
 autocorr = np.correlate(seq, seq,mode="full")
 
-# nr = (range(0,(2*len(seq)-1)))
-
-
-# Plot
-plt.figure(figsize=(11,4))
-plt.stem(autocorr)
+# Time lag vector for autocorrelation
+t_autocorr = np.linspace(-len(autocorr)/2, len(autocorr)/2, len(autocorr))
 
 # Plot the autocorrelation
+plt.figure(figsize=(11,4))
+plt.stem(t_autocorr,autocorr)
 plt.xlabel('Shift')
 plt.ylabel('Autocorrelation')
 plt.title('Autocorrelation of Gold code 3355A780')
