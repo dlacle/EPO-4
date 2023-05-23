865050d97d8df79387dcda4a61665486daca815d
Samh2oman <s.waterman@student.tudelft.nl>
2023-05-23 23:32:28 +0200

    uitbereiding localization

--- Localization.py
+++ Localization.py
@@ -4,10 +4,16 @@ import numpy as np
 # Define the positions of the 5 microphones
 mic_positions = np.array(
     [
-        [0.0, 0.0, 0.5],   # mic 1 (bottom left corner)
-        [0.0, 4.8, 0.5],   # mic 2 (top left corner)
-        [4.8, 4.8, 0.5],   # mic 3 (top right corner)
-        [4.8, 0.0, 0.5],   # mic 4 (bottom right corner)
-        [2.4, 4.8, 0.8]    # mic 5 (side)
+        [0  , 0  , 50],   # mic 1 (bottom left corner)
+        [0  , 480, 50],   # mic 2 (top left corner)
+        [480, 480, 50],   # mic 3 (top right corner)
+        [480, 0  , 50],   # mic 4 (bottom right corner)
+        [240, 480, 80]    # mic 5 (side)
     ]
 )
+for i, mic_position in enumerate(mic_positions):
+    locals()[f'x{i+1}'] = mic_position
+
+# test Print the variables
+for i in range(1, len(mic_positions) + 1):
+    print(f'x{i} = {locals()["x" + str(i)]}')
