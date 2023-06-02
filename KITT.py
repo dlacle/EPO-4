3814184753503bba4f74c9257ca99b0fdf6745d4
Zubair <zubair.alzubi@outlook.com>
2023-06-02 16:59:10 +0200

    .

--- KITT.py
+++ KITT.py
@@ -1,19 +1,29 @@
 import serial
-from Class_GUI import GUI
-class KITT(GUI):
-    def __init__(self, port): # called when the class is used to create a new object
-        # GUI()
-        self.serial = serial.Serial(port,baudrate=115200,rtscts=True)
-    def set_speed(self,speed):
+# from GUI import *
+
+
+class KITT:
+    def __init__(self, port):  # called when the class is used to create a new object
+        self.serial = serial.Serial(port, baudrate=115200, rtscts=True)
+
+    def set_speed(self, speed):
         pass
         # self.serial.write(......)
 
-    def set_angle(self,angle):
+    def set_angle(self, angle):
         pass
         # self.serial.write(......)
+
     def stop(self):
         self.set_angle(150)
         self.set_speed(150)
+
     def __del__(self):
         self.serial.close()
-KITT()
+
+
+car = KITT('COM7')
+print(car.serial)
+
+# print(KITT(3).port)
+# KITT()
