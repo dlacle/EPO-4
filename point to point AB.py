ff56a5ffffd96a4e8bc8d04b013c7a46044cb87b
RobertWetzels <93916885+RobertWetzels@users.noreply.github.com>
2023-06-22 21:31:36 +0200

    Update point to point AB.py

--- point to point AB.py
+++ point to point AB.py
@@ -4,15 +4,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 from KITT import*
 from epo4.Module2.Module2_mic_array.get_stationary_location import*
-from epo4.module5.methode2.ObstacleDetect import *
+
 def deg_to_pwm(angle):
-    MIN_PWM = 100
-    MAX_PWM = 200
     pwm_value = round(50 * angle * 1.1 / phi_max + MIN_PWM + 50)
     if pwm_value > 200:
-        pwm_value = MAX_PWM
+        pwm_value = 200
     elif pwm_value < 100:
-        pwm_value = MIN_PWM
+        pwm_value = 100
     return pwm_value
 
 
@@ -40,12 +38,15 @@ def measure_loc(t, v_old, m, Turn, L, alpha, power, old_x0, fa_max):
     PHI = phi(Turn, phi_max)
     phi_rad = np.radians(PHI)
     phi_rad = call_angle(phi_rad)
-    Fa = np.cos(phi_rad*(0.33*np.pi/np.radians(phi_max))) * acceleration_force(fa_max, power)
+    Fa = np.cos(phi_rad*(0.3*np.pi/np.radians(phi_max))) * acceleration_force(fa_max, power)
     a = acceleration(v_old, Fa, m)
     v = v_old + a * DT
+
     if phi_rad != 0:  # if the car turns
         R = radius(phi_rad, v)
         delta_theta = (v_old * np.sin(phi_rad) / L)
+        # delta_theta = DT*(v+v_old)/(2*R)
+        # print('dt', DT)
         theta = (delta_theta * DT)
         d_new = np.array([np.cos(alpha + theta), np.sin(alpha + theta)])
         x_new = np.array([R * (-np.sin(alpha) + np.sin(alpha + theta)),
@@ -54,6 +55,7 @@ def measure_loc(t, v_old, m, Turn, L, alpha, power, old_x0, fa_max):
         d_new = np.array([np.cos(alpha), np.sin(alpha)])
         x_new = old_x0 + (v+v_old)/2 * DT * d_new
         theta = 0
+
     X0 = x_new
     D0 = d_new
     Alpha = alpha + theta
@@ -176,9 +178,9 @@ def call_angle(angle):
 # Define constants
 # Fa_max = 10.615                 # Accelerating force Max.
 b = 3.81        # Constant for linear drag force
-c = 0.15         # Constant for quadratic drag force
+c = 0.3         # Constant for quadratic drag force
 Fa_max0 = b * 2.35 + c * pow(2.35, 2)       # Accelerating force Max.
-voltage = 19.7         # battery voltage of the car
+voltage = 20      # battery voltage of the car
 Fb_max = 17.7 * (voltage/17.7)**2                # Brake force Max.
 Fa_max = Fa_max0 * (voltage/17.7)**2
 m = 5.6         # Mass of car
@@ -189,26 +191,30 @@ phi_max = 28         # Max. steering angle
 R_min_forward = radius(np.radians(phi_max), 1)
 R_min_backward = radius(np.radians(phi_max), -1)
 
+MIN_PWM = 100
+MAX_PWM = 200
+
 # transmitting connection takes place over port 6
 comport = 'COM6'
-kitt = KITT(comport)    # create KITT object
-kitt.set_beacon()
+kitt = KITT(comport)
+kitt.set_beacon()       # create KITT object
 
 # determine begin and end location
 x0 = np.array([int(input('x0: ', )) / 100, int(input('y0: ', )) / 100])  # [x0,y0] starting location
-alpha = np.radians(int(input('starting orientaion: ', )))  # starting orientation angle with x-axis
+alpha = np.radians(int(input('starting orientation: ', )))  # starting orientation angle with x-axis
 d0 = np.array([np.cos(alpha), np.sin(alpha)])  # starting orientation vector
 x1 = np.array([int(input('x1: ', )) / 100, int(input('y1: ', )) / 100])  # [x1, y1] end location
+x2 = np.array([int(input('x2: ', )) / 100, int(input('y2: ', )) / 100])  # [x1, y1] end location
 
-# x0 = np.array([1, 1])  # [x0,y0] starting location
-# alpha = np.radians(90)  # starting orientation angle with x-axis
+# x0 = np.array([3.8, 3.8])  # [x0,y0] starting location
+# alpha = np.radians(180)  # starting orientation angle with x-axis
 # d0 = np.array([np.cos(alpha), np.sin(alpha)])  # starting orientation vector
-# x1 = np.array([1.3, 1])  # [x1, y1] end location
-# x2 = np.array([5, 5])  # [x1, y1] end location
+# x1 = np.array([2.4, 2.4])  # [x1, y1] end location
+# x2 = np.array([0.4, 0.4])  # [x1, y1] end location
 
-print('starting in:    5')
-time.sleep(1)
-print('starting in:    4')
+# print('starting in:    5')
+# time.sleep(1)
+# print('starting in:    4')
 time.sleep(1)
 print('starting in:    3')
 time.sleep(1)
@@ -226,6 +232,7 @@ Fa = 0
 # plot starting and end location
 plt.plot(x0[0], x0[1], marker='o')
 plt.plot(x1[0], x1[0], marker='o')
+plt.plot(x2[0], x2[1], marker='o')
 
 x_target = x1
 
@@ -234,61 +241,52 @@ dx, omega = relative_loc(x0, x_target, alpha)  # update end location relative to
 # drive from point to point
 while dx > 0.001:
     time.sleep(0.01)
-    if ObstacleDetect(x0, x1) == True:  # and not flag_obstacle
-        # flag_obstacle = True
-        dirr = np.arctan(d0[1] / d0[0])
-        dirr1 = np.arctan(d0[1] / d0[0]) + np.pi
-        print(dirr1)
-        if dirr > 0:
-            dis = 300000000
-            turn = deg_to_pwm(-30)
-            power = 158
-            print("current d", np.arctan(d0[1] / d0[0]))
-            print("desti", dirr - np.pi / 8)
-        elif dirr < 0:
-            turn = deg_to_pwm(30)
-            power = 158
-            print("current d", np.rad2deg(np.arctan(d0[1] / d0[0]) + np.pi))
-            print("desti", )
-            print(np.rad2deg(1.5 / 2))
-            plt.plot(x0[0], x0[1], marker='x')
 
     # barriers
-    # if x0[0] < 0.18 or x0[0] > 4.62 or x0[1] < 0.18 or x0[1] > 4.62:
-    #     kitt.brake(v)
-    #     # time.sleep(np.abs(v) / 2.3)
-    #     kitt.drive(150, 150)
-    #     dt, t, v, x0, d0, alpha = measure_loc(t, v, m, 150, L, alpha, 145, x0, Fb_max)
-    #     power = 142  # backward
-    #     turn = 150  # straight (but slightly turning against off-set, only noticeable when driving backward)
-    #
-    #     while x0[0] < 0.45 or x0[0] > 4.35 or x0[1] < 0.45 or x0[1] > 4.35:
-    #         dx, omega = relative_loc(x0, x_target, alpha)
-    #         turn = deg_to_pwm(np.rad2deg(-omega))
-    #         kitt.drive(power, turn)
-    #         dt, t, v, x0, d0, alpha = measure_loc(t, v, m, turn, L, alpha, power, x0, Fa_max)
+    if x0[0] < 0.18 or x0[0] > 4.62 or x0[1] < 0.18 or x0[1] > 4.62:
+        kitt.brake(v)
+        kitt.stop()
+        dt, t, v, x0, d0, alpha = measure_loc(t, v, m, 150, L, alpha, 145, x0, Fb_max)
+        power = 142  # backward
+        turn = 150  # straight (but slightly turning against off-set, only noticeable when driving backward)
+        while x0[0] < 0.45 or x0[0] > 4.35 or x0[1] < 0.45 or x0[1] > 4.35:
+            kitt.drive(power, turn)
+            dt, t, v, x0, d0, alpha = measure_loc(t, v, m, turn, L, alpha, power, x0, Fa_max)
 
-    # check if reachable driving forward
-    elif v <= 0 and dx < np.abs(np.sin(omega) * 2 * (R_min_forward+0.1)):
+    elif v <= 0 and dx < np.abs(np.sin(omega) * 2 * R_min_forward):
         power = 142  # backward
         turn = deg_to_pwm(np.rad2deg(-omega))
-        print('koek')
-        # if omega > 0:
-        #     turn = 100
-        # elif omega < 0:
-        #     turn = 200
-        # else:
-        #     turn = 150
         kitt.drive(power, turn)
     else:
         power = 158
         turn = deg_to_pwm(np.rad2deg(omega))
         kitt.drive(power, turn)
 
-    if dx < 0.1:
-        print('x2 reached')
+    if dx < 0.05 and np.any(x_target == x1):
+        print('x1 reached')
         kitt.brake(v)
         kitt.stop()
+        power = 150
+        turn = 150
+        v = 0
+        Location = get_stationary_location(10)
+        # Location = np.array([x0[0] + 0.1, x0[1] + 0.1])
+        x_model = x0
+        if Location[0] > x0[0] + 0.4 or Location[1] > x0[1] + 0.4:
+            x0 = Location
+        else:
+            x0 = np.array([(x_model[0] + Location[0]) / 2, (x_model[1] + Location[1]) / 2])
+        print('x0 ', x0)
+        dx, omega = relative_loc(x0, x_target, alpha)
+        if dx < 0.3:
+            input('press enter to start', )
+            x_target = x2
+        dt, t = set_time(start_time, t)
+        print('target', x_target)
+    elif dx < 0.1 and np.any(x_target == x2):
+        print('x2 reached')
+        kitt.brake(v)
+        kitt.stop()     # kitt.drive(150,150)
         power = 150
         turn = 150
         v = 0
